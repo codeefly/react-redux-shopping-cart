9d1a1d2f6fd186f89958933165fb273c672f3027
quixlab <quixlab.com@gmail.com>
2021-08-12 13:25:04 +0600

    shop reducer basic add

--- redux/shopping/shopping-reducer.js
+++ redux/shopping/shopping-reducer.js
@@ -1,36 +1,27 @@
 import * as actionTypes from "./shopping-types";
 
-export const addToCart = (itemID) => {
-    return {
-        type: actionTypes.ADD_TO_CART,
-        payload: {
-            id: itemID,
-        },
-    };
+const initial = {
+    products: [], // {id, title, desc, price, img}
+    cart: [],
+    currentItem: null,
 };
 
-export const removeFromCart = (itemID) => {
-    return {
-        type: actionTypes.REMOVE_FROM_CART,
-        payload: {
-            id: itemID,
-        },
-    };
+const shopReducer = (state = initial, action) => {
+    switch (action.tpe) {
+        case actionTypes.ADD_TO_CART:
+            return {};
+
+        case actionTypes.REMOVE_FROM_CART:
+            return {};
+
+        case actionTypes.ADJUST_ITEM_QTY:
+            return {};
+        case actionTypes.LOAD_CURRENT_ITEM:
+            return {};
+
+        default:
+            return state;
+    }
 };
 
-export const adjustQty = (itemID, value) => {
-    return {
-        type: actionTypes.ADJUST_ITEM_QTY,
-        payload: {
-            id: itemID,
-            qty: value,
-        },
-    };
-};
-
-export const loadCurrentItem = (item) => {
-    return {
-        type: actionTypes.LOAD_CURRENT_ITEM,
-        payload: item,
-    };
-};
+export default shopReducer;
